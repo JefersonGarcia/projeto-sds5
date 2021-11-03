fdc4d8e6de87097715e9a84b4fdfd1d43c502bf0
JefersonGarcia <jeferson87.ti@gmail.com>
2021-11-03 17:16:02 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/garcia/backend/entities/Sale.java
@@ -0,0 +1,34 @@
+package com.garcia.backend.entities;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_sales")
+@Data
+@NoArgsConstructor
+public class Sale {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Integer visited;
+	private Integer deals;
+	private Double amount;
+	private LocalDate date;
+	
+	@ManyToOne
+	@JoinColumn(name = "seller_id")
+	private Seller seller;
+}
---
+++ backend/src/main/java/com/garcia/backend/entities/Seller.java
@@ -0,0 +1,35 @@
+package com.garcia.backend.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_sellers")
+@Data
+@NoArgsConstructor
+public class Seller {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	@Getter
+	@OneToMany(mappedBy = "seller")
+	private List<Sale> sales = new ArrayList<>();
+	
+}
